1a1cfce395983abae2a81a5b7af630ac1b2d8271
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2018-03-22 11:00:03 +1000

    create welcome page

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,28 +3,22 @@ import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { AppCustomPreloader } from '../app-custom-preloader';
 
 import { CommonModule } from '@angular/common';
-import { PageNavigationComponent } from '../page-navigation/page-navigation.component';
-import { PageAboutComponent } from '../page-about/page-about.component';
-import { PageContactComponent } from '../page-contact/page-contact.component';
-import { PagePressComponent } from '../page-press/page-press.component';
-import { PageLandingComponent } from 'app/page-landing/page-landing.component';
-import { PageSolutionComponent } from '../page-solution/page-solution.component';
-import { PageWelcomeComponent } from '../page-welcome/page-welcome.component';
-
+import { WelcomeComponent } from '../pages/welcome/welcome.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/welcome/landingpage', pathMatch: 'full' },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  // { path: '', redirectTo: '/welcome/landingpage', pathMatch: 'full' },
   {
-    path: 'welcome', component: PageWelcomeComponent,
-    children: [
-      { path: 'landingpage', component: PageLandingComponent },
-      { path: 'solutionpage', component: PageSolutionComponent, },
-      { path: 'presspage', component: PagePressComponent, data: { preload: true } },
-      { path: 'aboutpage', component: PageAboutComponent, data: { preload: true } },
-      { path: 'contactpage', component: PageContactComponent },
-    ]
+    path: 'welcome', component: WelcomeComponent,
+    // children: [
+    //   { path: 'landingpage', component: PageLandingComponent },
+    //   { path: 'solutionpage', component: PageSolutionComponent, },
+    //   { path: 'presspage', component: PagePressComponent, data: { preload: true } },
+    //   { path: 'aboutpage', component: PageAboutComponent, data: { preload: true } },
+    //   { path: 'contactpage', component: PageContactComponent },
+    // ]
   },
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: AppCustomPreloader })],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { MouseWheelDirective } from './mousewheel.directive';
 
+import { D3Service } from 'd3-ng2-service';
+import { LoaderService } from './services/loader.service';
+
+import 'hammerjs';
+import 'hammer-timejs';
+export class MyHammerConfig extends HammerGestureConfig {
+
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    MouseWheelDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    D3Service,
+    LoaderService,
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
