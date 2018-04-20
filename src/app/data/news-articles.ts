17b2b253c301e00ad7b8a45d4efd8b2703389c80
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2018-04-20 10:18:29 +1000

    insert images to about page news section

--- src/app/data/news-articles.ts
+++ src/app/data/news-articles.ts
@@ -4,13 +4,13 @@ export const newsArticle: NEWSARTICLE[] = [
     {
         title: 'Komatsu and Unearthed unveil four tech innovators to watch out for in 2018',
         date: '1 March 2018',
-        imgURL: 'assets/images/press/unearthed-logo-300.png',
-        imgALT: 'unearthed logo'
+        imgURL: '',
+        imgALT: ''
     },
     {
         title: 'Komatsu engages with startups like a well-oiled machine',
         date: '1 March 2018',
-        imgURL: '',
+        imgURL: 'assets/images/press/unearthed-logo-300.png',
         imgALT: '',
     },
     {
@@ -22,31 +22,31 @@ export const newsArticle: NEWSARTICLE[] = [
     {
         title: 'Ready for impact - four startups graduate Unearthed Accelerator 2017 at Demo Day',
         date: '1 December 2017',
-        imgURL: '',
+        imgURL: 'assets/images/press/unearthed-logo-300.png',
         imgALT: '',
     },
     {
         title: 'Why did five tech startups take a bus to mining operation in Queensland?',
         date: '3 November 2017',
-        imgURL: '',
+        imgURL: 'assets/images/press/unearthed-logo-300.png',
         imgALT: '',
     },
     {
         title: 'Five tech startups accelerate towards QUeensland coal mine',
         date: '8 October 2017',
-        imgURL: '',
+        imgURL: 'assets/images/press/unearthed-logo-300.png',
         imgALT: '',
     },
     {
         title: 'Unearthed Accelerator Program kicks off',
         date: '19 July 2017',
-        imgURL: '',
+        imgURL: 'assets/images/press/austmininglogo.png',
         imgALT: '',
     },
     {
         title: 'Meet the six Accelerator teams',
         date: '13 July 2017',
-        imgURL: '',
+        imgURL: 'assets/images/press/austmine.png',
         imgALT: '',
     },
 ];
