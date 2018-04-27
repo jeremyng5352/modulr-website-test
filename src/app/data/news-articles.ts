d7fed9bbfd3d802c71805a4784f41cc2cce64ea3
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2018-04-27 15:10:09 +1000

    add alt content for images

--- src/app/data/news-articles.ts
+++ src/app/data/news-articles.ts
@@ -5,48 +5,48 @@ export const newsArticle: NEWSARTICLE[] = [
         title: 'Komatsu and Unearthed unveil four tech innovators to watch out for in 2018',
         date: '1 March 2018',
         imgURL: '',
-        imgALT: ''
+        imgALT: 'MiningGlobal logo'
     },
     {
         title: 'Komatsu engages with startups like a well-oiled machine',
         date: '1 March 2018',
         imgURL: 'assets/images/press/unearthed-logo-300.png',
-        imgALT: '',
+        imgALT: 'UnearthedSolutions logo',
     },
     {
         title: 'Ready for impace - four tech startups graduate Unearthed Accelerator 2017 at Demo Day',
         date: '8 December 2017',
-        imgURL: '',
-        imgALT: '',
+        imgURL: 'assets/images/other_company/Mining_Com.png',
+        imgALT: 'Mining.com logo',
     },
     {
         title: 'Ready for impact - four startups graduate Unearthed Accelerator 2017 at Demo Day',
         date: '1 December 2017',
         imgURL: 'assets/images/press/unearthed-logo-300.png',
-        imgALT: '',
+        imgALT: 'UnearthedSolutions logo',
     },
     {
         title: 'Why did five tech startups take a bus to mining operation in Queensland?',
         date: '3 November 2017',
         imgURL: 'assets/images/press/unearthed-logo-300.png',
-        imgALT: '',
+        imgALT: 'UnearthedSolutions logo',
     },
     {
         title: 'Five tech startups accelerate towards QUeensland coal mine',
         date: '8 October 2017',
         imgURL: 'assets/images/press/unearthed-logo-300.png',
-        imgALT: '',
+        imgALT: 'UnearthedSolutions logo',
     },
     {
         title: 'Unearthed Accelerator Program kicks off',
         date: '19 July 2017',
         imgURL: 'assets/images/press/austmininglogo.png',
-        imgALT: '',
+        imgALT: 'AustralianMining logo',
     },
     {
         title: 'Meet the six Accelerator teams',
         date: '13 July 2017',
         imgURL: 'assets/images/press/austmine.png',
-        imgALT: '',
+        imgALT: 'AustMine logo',
     },
 ];
